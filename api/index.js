cc93e89cfb2144a89abdf813080efd5373e64616
Camilo Segura <camilosegurar@gmail.com>
2020-02-11 07:36:55 -0500

    Removed SSR

--- api/index.js
+++ api/index.js
@@ -1,18 +1,15 @@
 import axios from 'axios';
-import getConfig from 'next/config';
 
-const { publicRuntimeConfig: { PORT, HOST } } = getConfig();
-console.log("PORT", PORT, HOST)
 function getProducts() {
-  return axios.get(`${HOST}:${PORT}/api/products`).then((response) => response.data);
+  return axios.get('/api/products').then((response) => response.data);
 }
 
 function getProductAvailability(id) {
-  return axios.get(`${HOST}:${PORT}/api/products/${id}/availability`).then((response) => response.data);
+  return axios.get(`/api/products/${id}/availability`).then((response) => response.data);
 }
 
 function getProductInfo(id) {
-  return axios.get(`${HOST}:${PORT}/api/products/${id}`).then((response) => response.data);
+  return axios.get(`/api/products/${id}`).then((response) => response.data);
 }
 
 export {
